0e53d6149a5ac48277398955c581b6ed7e3ce0a1
rex <167688826+30rex30@users.noreply.github.com>
2025-03-24 20:45:50 +0000

    Mostrar a stora

--- acabar em casa.cpp
+++ acabar em casa.cpp
@@ -3,33 +3,88 @@
 
 - Turma
 - Nome
-- N˙mero
+- N√∫mero
 - Nota de PSI
 
-O programa dever· mostrar quantos alunos est„o 'aprovados' ‡ disciplina e
-quantos est„o 'reprovados'. Dever· ainda mostrar o 'nome do aluno' que
-teve a melhor nota escrevendo a seguinte mensagem (ìParabÈns XXXXXXX!
-…s o maior!î).
+O programa dever√° mostrar quantos alunos est√£o 'aprovados' √† disciplina e 
+quantos est√£o 'reprovados'. Dever√° ainda mostrar o 'nome do aluno' que
+teve a melhor nota escrevendo a seguinte mensagem (‚ÄúParab√©ns XXXXXXX!
+√âs o maior!‚Äù).
 
 */
-
+//----------------------------
+// Nota Max = 20
+//
+// Nota Min = 1
+//
+//----------------------------
 #include <iostream>
-#include <string>
+#include <string.h>
+#include <windows.h>
+#include <stdlib.h>
+
+
 using namespace std;
 
-struct aluno{
+struct Aluno{
+	string name;
 	string truma;
-	char name;
-	int n;
+	int n; // numero 
 	int nota_psi;
+	
 };
 
-	for(int i=0;i<5;i++)
+
+int main()
+{
+	// ler valores 
+	Aluno aluno[5];
+	for(int i=0; i < 5 ; i++)
 	{
-		cout<<"Aluno: "<< i + 1 <<endl;
-		cout<<"Truma"<<endl;
-		getline(cin,aluno[i].truma);
-		cout<<"Nome"<<endl;
-		cin>>aluno[i]
+		cout<<"Aluno "<< i + 1 <<endl;
+		cout<<"Nome:"<<endl;
+		getline(cin, aluno[i].name);
+		cout<<"Truma:"<<endl;
+		getline(cin, aluno[i].name);
+		cout<<"Numero:"<<endl;
+		cin>>aluno[i].n;
+		cout<<"Nota a PSI:"<<endl;
+		cin>>aluno[i].nota_psi;
+		cin.ignore();
+		system("cls");
 		
+
 	}
+		int P=0; // contar as positivas 
+		int N=0; // contar as negativas
+		int melhor_aluno=0;
+		string maior_nota;
+		
+	
+		for(int i=0; i < 5; i++)
+		{
+			if(aluno[i].nota_psi >=10 )
+			{	
+				
+			P = P + 1;
+			
+			}
+			
+			if(aluno[i].nota_psi <=8) // 8 e o valor max da negativa 
+			{
+				N = N + 1;
+			}
+		}
+		cout<<"O numero de Positivas: "<< P <<endl;
+		cout<<"O numero de negativas: "<< N <<endl;
+		
+			for(int i=0; i  < 5; i++)
+			{
+				if(aluno[i].nota_psi > aluno[i].nota_psi)
+				{
+					
+				}
+			}
+
+	
+}
